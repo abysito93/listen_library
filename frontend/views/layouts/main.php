b2be74604c126caf96382bb225fc9f51cb72f4c5
Samuel Reyes Roldán <samuel.reyes@upr.edu>
2017-03-10 15:15:18 -0800

    app

---
+++ frontend/views/layouts/main.php
@@ -0,0 +1,115 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\AppAsset;
+use common\widgets\Alert;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+
+    <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+
+  .carousel-inner img {
+      width: 100%; /* Set width to 100% */
+      margin: auto;
+      min-height:200px;
+  }
+
+  /* Hide the carousel text when the screen is less than 600 pixels wide */
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none;
+    }
+  }
+  </style>
+
+
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => 'Listen Library',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    $menuItems = [
+        ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Library', 'url' => ['#']],
+        ['label' => 'Browse', 'url' => ['#']],
+        //['label' => 'About', 'url' => ['/site/about']],
+        //['label' => 'Contact', 'url' => ['/site/contact']],
+    ];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = '<li>'
+            . Html::beginForm(['/site/logout'], 'post')
+            . Html::submitButton(
+                'Logout (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link logout']
+            )
+            . Html::endForm()
+            . '</li>';
+    }
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="container-fluid text-center"
+ style="color:white;
+ background-color:black;">
+
+        <p> Listen Library </p>
+
+
+
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ frontend/views/site/AnotherIndex.php
@@ -0,0 +1,68 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="http://g-ecx.images-amazon.com/images/G/01/Audible/en_US/images/creative/2328_FantasticBeast_ZingDT_1880x800_06._CB536359087_.jpg" alt="Image">
+        <div class="carousel-caption">
+
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://g-ecx.images-amazon.com/images/G/01/Audible/en_US/images/creative/2381_ForgersLife_ZingDT_1880x800._CB534068072_.jpg Image Maybe" alt="Image">
+        <div class="carousel-caption">
+
+
+        </div>
+      </div>
+
+
+
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<div class="container text-center">
+  <h3>Best Sellers </h3>
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="http://ecx.images-amazon.com/images/I/611zbBzG3XL._SL300_.jpg" class="img-responsive" style="width:100%" alt="Image">
+<p> Miss Peregrine's Home for Peculiar Children<p>
+<p> Written by: Ransom Riggs <p>
+<p> Narrated by: Jesse Bernstein <p>
+    </div>
+    <div class="col-sm-4">
+      <img src="http://ecx.images-amazon.com/images/I/51sakao0D0L._SL300_.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <p> Trump: The Art of the Deal<p>
+      <p> Written by: Donald J. Trump , Tony Schwartz <p>
+      <p> Narrated by: Kaleo Griffith , Donald J. Trump <p>
+
+    </div>
+    <div class="col-sm-4">
+      <img src="http://ecx.images-amazon.com/images/I/51ejXdSceNL._SL300_.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <p> The Power of Habit: Why We Do What We Do in Life and Business<p>
+      <p> Written by: Charles Duhigg <p>
+      <p> Narrated by: Mike Chamberlain <p>
+
+      </div>
+    </div>
+  </div>
+</div><br>
